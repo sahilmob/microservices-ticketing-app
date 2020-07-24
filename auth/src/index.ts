e3a1e0e6d1616e66807222498bb4d31bc7be5113
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-07-24 18:00:34 +0300

    create not found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+
+app.get("*", () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
