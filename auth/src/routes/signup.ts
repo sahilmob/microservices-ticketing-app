c98898da3e53293056a79a134779e07e36158a27
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-07-24 13:38:56 +0300

    add email validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,22 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Hello there!");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 30 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    res.send("Hello there!");
+  }
+);
 
 export { router as signupRouter };
