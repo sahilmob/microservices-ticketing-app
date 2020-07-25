b71a42409bbabd834ddfc11eec029c83e1d5bd87
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-07-26 00:11:23 +0300

    extract current user to a middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,20 +1,11 @@
 import express from "express";
-import jwt from "jsonwebtoken";
+
+import { currentUSer } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    console.log(payload);
-    res.send({ currentUser: payload });
-  } catch (error) {
-    return res.send({ currentUser: null });
-  }
+router.get("/api/users/currentuser", currentUSer, (req, res) => {
+  return res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
