b8edfff76dd0a3c2af54e5c451e4b8c0fc5a262b
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-07-25 23:37:00 +0300

    remove unused import

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,7 +2,6 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
-import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
 import { User } from "../models/user";
 import { validateRequest } from "../middlewares/validate-request";
