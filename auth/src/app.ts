0d0c3dadaf8ea3773461eecd0873a85bdfc19d42
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-07-26 22:30:01 +0300

    setup testing env.

---
+++ auth/src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import cookieSession from "cookie-session";
+import "express-async-errors";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.all("*", async (_req, _res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
