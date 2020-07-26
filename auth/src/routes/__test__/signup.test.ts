67ef5ed80d6f08723bbdd0c17a5de52032cd19d8
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-07-26 23:03:34 +0300

    test Set-Cookie header

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -52,3 +52,12 @@ it("disallows duplicate emails", async () => {
     })
     .expect(400);
 });
+
+it("sets a cookie after successful signup", async () => {
+  const response = await request(app).post("/api/users/signup").send({
+    email: "test@test.com",
+    password: "password",
+  });
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
