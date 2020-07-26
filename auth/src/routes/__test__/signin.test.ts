c1d98d92dc32ea6d205a69e5f2f9ea697737fdc6
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-07-26 23:10:25 +0300

    testing signin

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,56 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("fails when an email that does not exist is supplied", async () => {
+  return request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("fails when an incorrect password is supplied", async () => {
+  await request(app).post("/api/users/signup").send({
+    email: "test@test.com",
+    password: "password",
+  });
+
+  return request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password1",
+    })
+    .expect(400);
+});
+
+it("returns a 200 on successful signup", async () => {
+  await request(app).post("/api/users/signup").send({
+    email: "test@test.com",
+    password: "password",
+  });
+
+  return request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(200);
+});
+
+it("sets a cookie after successful signin", async () => {
+  await request(app).post("/api/users/signup").send({
+    email: "test@test.com",
+    password: "password",
+  });
+
+  const response = await request(app).post("/api/users/signin").send({
+    email: "test@test.com",
+    password: "password",
+  });
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
