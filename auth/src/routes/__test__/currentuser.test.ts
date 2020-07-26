78f951da3d51d6a708cc58e11982c6c94f4a9b79
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-07-26 23:49:33 +0300

    pul signup in the global scope

--- auth/src/routes/__test__/currentuser.test.ts
+++ auth/src/routes/__test__/currentuser.test.ts
@@ -2,15 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("returns details about the current user", async () => {
-  const signupResponse = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201);
-
-  const cookie = signupResponse.get("Set-Cookie");
+  const cookie = await global.signup();
 
   const response = await request(app)
     .get("/api/users/currentuser")
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,16 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
+
 import { app } from "../app";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signup(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 
@@ -28,3 +38,20 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signup = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
