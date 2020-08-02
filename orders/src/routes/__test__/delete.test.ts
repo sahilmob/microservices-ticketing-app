2a943d9d952652da05dacfaa715bc64b3c297eea
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-08-02 16:51:53 +0300

    implement/test order delete (cancel)

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,59 @@
+import request from "supertest";
+import { OrderStatus } from "@smtickets1/common";
+
+import { app } from "../../app";
+import { Ticket } from "../../models";
+
+const createTicket = async (price = 10, title = "concert") => {
+  const newTicket = Ticket.build({
+    price,
+    title,
+  });
+
+  await newTicket.save();
+
+  return newTicket;
+};
+
+it("allows the user to delete his orders only", async () => {
+  const userId1 = "5555555";
+  const userId2 = "6666666";
+  const cookie1 = await global.signup(userId1);
+  const cookie2 = await global.signup(userId2);
+  const ticket1 = await createTicket();
+  const ticket2 = await createTicket();
+
+  const {
+    body: { id: order1Id },
+  } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", cookie1)
+    .send({
+      ticketId: ticket1.id,
+    })
+    .expect(201);
+
+  const {
+    body: { id: order2Id },
+  } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", cookie2)
+    .send({
+      ticketId: ticket2.id,
+    })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order1Id}`)
+    .set("Cookie", cookie2)
+    .send({})
+    .expect(401);
+
+  const { body } = await request(app)
+    .delete(`/api/orders/${order2Id}`)
+    .set("Cookie", cookie2)
+    .send({})
+    .expect(200);
+
+  expect(body.status).toBe(OrderStatus.Cancelled);
+});
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,33 @@
 import express, { Request, Response } from "express";
+import {
+  requireAuth,
+  NotFoundError,
+  NotAuthorized,
+  OrderStatus,
+} from "@smtickets1/common";
+import { Order } from "../models";
 
 const router = express.Router();
 
-router.delete("/api/orders/:id", (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:id",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { currentUser, params } = req;
+    const { id } = params;
+
+    const order = await Order.findById(id).populate("ticket");
+
+    if (!order) throw new NotFoundError();
+
+    if (order.userId !== currentUser!.id) throw new NotAuthorized();
+
+    order.status = OrderStatus.Cancelled;
+
+    await order.save();
+
+    res.send(order);
+  }
+);
 
 export { router as deleteRouter };
