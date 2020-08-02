9f02a27ab5ea82fafc8ceb08e819501f5202b930
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-08-02 16:14:41 +0300

    implement/test getting user orders

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,70 @@
+import request from "supertest";
+
+import { app } from "../../app";
+import { Ticket, OrderDoc } from "../../models";
+
+const createTicket = async (price = 10, title = "concert") => {
+  const newTicket = Ticket.build({
+    price,
+    title,
+  });
+
+  await newTicket.save();
+
+  return newTicket;
+};
+
+it("returns user orders", async () => {
+  const userId1 = "5555555";
+  const userId2 = "6666666";
+  const cookie1 = await global.signup(userId1);
+  const cookie2 = await global.signup(userId2);
+  const ticket1 = await createTicket();
+  const ticket2 = await createTicket();
+  const ticket3 = await createTicket();
+  const ticket4 = await createTicket();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", cookie1)
+    .send({
+      ticketId: ticket1.id,
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", cookie1)
+    .send({
+      ticketId: ticket2.id,
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", cookie2)
+    .send({
+      ticketId: ticket3.id,
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", cookie2)
+    .send({
+      ticketId: ticket4.id,
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .get("/api/orders")
+    .set("Cookie", cookie2)
+    .send({})
+    .expect(200);
+
+  expect(response.body.length).toBe(2);
+  const areOrdersForTheSameUser = response.body.every(
+    (order: OrderDoc) => order.userId === userId2
+  );
+  expect(areOrdersForTheSameUser).toBe(true);
+});
--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from "express";
+import { requireAuth } from "@smtickets1/common";
+
+import { Order } from "../models";
 
 const router = express.Router();
 
-router.get("/api/orders", (req: Request, res: Response) => {
-  res.send({});
+router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
+  const { currentUser } = req;
+
+  const userOrders = await Order.find({
+    userId: currentUser!.id,
+  }).populate("ticket");
+
+  res.send(userOrders);
 });
 
 export { router as indexRouter };
