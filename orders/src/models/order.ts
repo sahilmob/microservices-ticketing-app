27715f71c2b6728ebcb0689a0b87a55448872f35
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-08-02 16:13:35 +0300

    export userDoc interface

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -10,7 +10,7 @@ interface OrderAttrs {
   ticket: TicketDoc;
 }
 
-interface OrderDoc extends mongoose.Document {
+export interface OrderDoc extends mongoose.Document {
   userId: string;
   status: OrderStatus;
   expiresAt: Date;
