0d02fbc4ff80bcec6e53c19b599024d591ace12e
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-08-02 15:43:23 +0300

    implement/test creating orders

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,62 @@
+import request from "supertest";
+import mongoose from "mongoose";
+import { OrderStatus } from "@smtickets1/common";
+
+import { app } from "../../app";
+import { Ticket, Order } from "../../models";
+
+it("returns an error if the ticket doesn't exist", async () => {
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signup())
+    .send({
+      ticketId: new mongoose.Types.ObjectId(),
+    })
+    .expect(404);
+});
+
+it("returns an error if the ticket is already reserved", async () => {
+  const newTicket = Ticket.build({
+    price: 10,
+    title: "concert",
+  });
+
+  await newTicket.save();
+
+  const expiry = new Date();
+  expiry.setSeconds(expiry.getSeconds() + 15 * 60);
+
+  const newOrder = Order.build({
+    status: OrderStatus.Created,
+    expiresAt: expiry,
+    userId: "1234",
+    ticket: newTicket,
+  });
+
+  await newOrder.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signup())
+    .send({
+      ticketId: newTicket.id,
+    })
+    .expect(400);
+});
+
+it("reserves a ticket", async () => {
+  const newTicket = Ticket.build({
+    price: 10,
+    title: "concert",
+  });
+
+  await newTicket.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signup())
+    .send({
+      ticketId: newTicket.id,
+    })
+    .expect(201);
+});
---
+++ orders/src/routes/new.ts
@@ -0,0 +1,56 @@
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  OrderStatus,
+  BadRequestError,
+} from "@smtickets1/common";
+import { body } from "express-validator";
+
+import { Order, Ticket } from "../models";
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
+const router = express.Router();
+
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("Ticket id must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) throw new NotFoundError();
+
+    const isReserved = await ticket.isReserved();
+
+    if (isReserved) throw new BadRequestError("Ticket is already reserved");
+
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    const newOrder = Order.build({
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      userId: req.currentUser!.id,
+      ticket,
+    });
+
+    await newOrder.save();
+
+    return res.status(201).send(newOrder);
+  }
+);
+
+export { router as newRouter };
