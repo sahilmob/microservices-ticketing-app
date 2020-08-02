84217cd2c385e5b1b957b47e51b5f0f64d32f881
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-08-02 03:02:04 +0300

    add isReserved method

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import { OrderStatus } from "@smtickets1/common";
+
+import { Order } from "./order";
 
 interface TicketAttrs {
   title: string;
@@ -8,6 +11,7 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -39,6 +43,21 @@ const ticketSchema = new mongoose.Schema(
 
 ticketSchema.statics.build = (attars: TicketAttrs) => {
   return new Ticket(attars);
+};
+
+ticketSchema.methods.isReserved = async function () {
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+
+  return !!existingOrder;
 };
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
