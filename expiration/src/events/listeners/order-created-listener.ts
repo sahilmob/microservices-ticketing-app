aaba4119ade5c520001f4f0db88f3375231d03f0
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2020-08-06 00:21:54 +0300

    add a job on ticket created

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -3,10 +3,14 @@ import { Message } from "node-nats-streaming";
 import {} from "bull";
 
 import { queueGroupName } from "./";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   readonly queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    await expirationQueue.add({ orderId: data.id });
+    msg.ack();
+  }
 }
