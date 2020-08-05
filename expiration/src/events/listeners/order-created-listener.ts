18902f9ad6ea2c9dc385edf9052c7450d0a6f266
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2020-08-06 00:38:16 +0300

    delay adding to queue

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -10,7 +10,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    await expirationQueue.add({ orderId: data.id });
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    await expirationQueue.add(
+      { orderId: data.id },
+      {
+        delay,
+      }
+    );
     msg.ack();
   }
 }
