11306b6dadf6004aab037569861e6994ddadf904
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2020-08-06 01:22:25 +0300

    populate ticket field

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -21,7 +21,7 @@ export class ExpirationCompleteListener extends Listener<
 
   async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
     const { orderId } = data;
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate("ticket");
 
     if (!order) throw new NotFoundError();
 
