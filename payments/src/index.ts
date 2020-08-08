11e599aaf4a440a73cb8c83ae78aa0259b83d5df
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2020-08-08 16:06:48 +0300

    listen to order-cancelled event

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
-import { OrderCreatedListener } from "./events/listeners";
+import {
+  OrderCreatedListener,
+  OrderCancelledListener,
+} from "./events/listeners";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -46,6 +49,7 @@ const start = async () => {
     console.log("Database connected");
 
     new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
 
     app.listen(3000, () => console.log("Listening on port 3000!"));
   } catch (error) {
